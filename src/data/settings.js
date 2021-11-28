2d1cb0fa83437ce695659165d04dcdb20e37a4ea
Jesse Sivonen <j.sivonen@hotmail.com>
2021-11-28 12:22:31 +0200

    Load plenti-cms.json from assets to support Plenti

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,5 +1,5 @@
 export async function getSettings() {
-    const response = await fetch('/plenti-cms.json');
+    const response = await fetch('/assets/plenti-cms.json');
     const settings = await response.json();
     return settings;
 }
